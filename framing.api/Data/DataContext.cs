4da801c07a9629487deaec165f39aa680135cb0e
Tommy <tommycrabtree@outlook.com>
2020-06-30 16:10:33 -0400

    Initial Commit

---
+++ framing.api/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using framing.api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace framing.api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Value> Values { get; set; }
+    }
+}
